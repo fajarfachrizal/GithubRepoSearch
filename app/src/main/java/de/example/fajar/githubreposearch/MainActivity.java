76dee3b9f631a51bb3eac7264ab2a5e3bdca80d3
fajarfachrizal <fajar.fachrizal@gmail.com>
2018-10-05 00:33:30 +0200

    add menu
    add menu, implement code for onCreateOptionMenu() and onOptionsItemSelected()

--- app/src/main/java/de/example/fajar/githubreposearch/MainActivity.java
+++ app/src/main/java/de/example/fajar/githubreposearch/MainActivity.java
@@ -1,9 +1,13 @@
 package de.example.fajar.githubreposearch;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -11,7 +15,7 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
 
-    @BindView(R.id.et_search_box) TextView mSearchBoxEditText;
+    @BindView(R.id.et_search_box) EditText mSearchBoxEditText;
     @BindView(R.id.tv_url_display) TextView mUrlDisplayTextView;
     @BindView(R.id.tv_github_search_result_json) TextView mSearchResultsTextView;
 
@@ -23,7 +27,24 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItemThatWasSelected = item.getItemId();
+        if(menuItemThatWasSelected == R.id.action_search){
+            Context context = MainActivity.this;
+            String message = getString(R.string.toast_massage);
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
